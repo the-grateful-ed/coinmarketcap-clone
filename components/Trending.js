46581176f216e20a656359ee143e374733e6bf55
the-grateful-ed <the-grateful-ed@riseup.net>
2022-05-12 19:55:39 +0000

    setup and components

---
+++ components/Trending.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import fire from '../assets/fire.png'
+import btc from '../assets/btc.png'
+import usdt from '../assets/usdt.png'
+import gainers from '../assets/gainers.png'
+import recent from '../assets/recent'
+import ReactSwitch from 'react-switch'
+
+const Trending = () => {
+    const [checked, setChecked] = useState(false)
+  return (
+    <div className='text-white'>
+    <div className={styles.trendingWrapper}
+    Trending
+    </div>
+  )
+}
+
+export default Trending
