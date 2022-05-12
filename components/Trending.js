8decc236e62284361444de4c50d194dee47b7d3e
the-grateful-ed <the-grateful-ed@riseup.net>
2022-05-12 22:41:34 +0000

    setup header, setup trending

--- components/Trending.js
+++ components/Trending.js
@@ -1,28 +1,33 @@
 import React, { useState } from 'react'
+import ReactSwitch from 'react-switch'
 import fire from '../assets/fire.png'
 import btc from '../assets/btc.png'
 import usdt from '../assets/usdt.png'
 import gainers from '../assets/gainers.png'
-import recent from '../assets/recent'
-import ReactSwitch from 'react-switch'
+import recent from '../assets/recent.png'
 
 const styles = {
-    trendingWrapper: 'mx-auto'
+  trendingWrapper: 'mx-auto',
 }
 
-const Trending = () => {
-    const [checked, setChecked] = useState(false)
+function Trending() {
+  const [checked, setChecked] = useState(false)
   return (
-    <div className='text-white'>
-        <div className={styles.trendingWrapper}>
-            <div className='flex justify-between'>
-                <h1 className={styles.h1}>Todays Crypto Prices by Market Cap</h1>
-            <div className='flex'>
-                <p className='text-gray-400'>Highlights &nbsp</p>
-                <ReactSwitch checked={checked} onChange={() => {setChecked(!checked)}}/>
-            </div>
-            </div>
+    <div className="text-white">
+      <div className={styles.trendingWrapper}>
+        <div className="flex justify-between">
+          <h1 className={styles.h1}>Todays Crypto Prices by Market Cap</h1>
+          <div className="flex">
+            <p className="text-gray-400">Highlights &nbsp</p>
+            <ReactSwitch
+              checked={checked}
+              onChange={() => {
+                setChecked(!checked)
+              }}
+            />
+          </div>
         </div>
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
+// import CMCtable from '../components/cmc-table/cmcTable'
 import React from 'react'
 import Header from '../components/Header'
 import Trending from '../components/Trending'
+// import SwapCryptoModal from '../components/swapCryptoModal'
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <div className='min-x-screen'> 
+    // eslint-disable-next-line react/jsx-filename-extension
+    <div className="min-h-screen">
       <Header />
-        <div className='mt-10'/>
-        <Trending />
-        <div className='mt-20'/>
-        {/* <CMCTable /> */}
+      {/* <SwapCryptoModal /> */}
+      <div className="mt-10" />
+      <Trending />
+      <div className="mt-20" />
+      {/* <CMCtable /> */}
     </div>
   )
 }
-
-export default Home
