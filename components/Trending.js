c9a34e7256e497b0ee5ff8e37495bf4835ce0d01
the-grateful-ed <the-grateful-ed@riseup.net>
2022-05-12 20:12:57 +0000

    Trending

--- components/Trending.js
+++ components/Trending.js
@@ -6,12 +6,23 @@ import gainers from '../assets/gainers.png'
 import recent from '../assets/recent'
 import ReactSwitch from 'react-switch'
 
+const styles = {
+    trendingWrapper: 'mx-auto'
+}
+
 const Trending = () => {
     const [checked, setChecked] = useState(false)
   return (
     <div className='text-white'>
-    <div className={styles.trendingWrapper}
-    Trending
+        <div className={styles.trendingWrapper}>
+            <div className='flex justify-between'>
+                <h1 className={styles.h1}>Todays Crypto Prices by Market Cap</h1>
+            <div className='flex'>
+                <p className='text-gray-400'>Highlights &nbsp</p>
+                <ReactSwitch checked={checked} onChange={() => {setChecked(!checked)}}/>
+            </div>
+            </div>
+        </div>
     </div>
   )
 }
